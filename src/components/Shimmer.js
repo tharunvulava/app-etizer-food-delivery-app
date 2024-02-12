4d889cf716c299fda074b0ef1e6c2dae5099ff5e
Vulava Tharun <tharunvulava@gmail.com>
2024-02-12 20:23:07 +0530

    Shimmer ui updated

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,18 +1,28 @@
 const Shimmer = () => {
   return (
-    <div className="shimmer-container">
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
-      <div className="shimmer-card"></div>
+    <div className="body">
+      <div className="flex flex-col justify-center items-center m-4 p-4">
+        <div>
+          <input
+            type="text"
+            className="border border-solid border-black px-4 py-2  rounded-lg w-80"
+          ></input>
+          <button className="px-4 py-2 m-2 bg-green-500 text-white rounded-lg"></button>
+        </div>
+      </div>
+
+      <div className="flex flex-wrap ">
+        <div className="m-4 p-4 w-[219px] h-[340px] bg-red-200 hover:bg-red-300"></div>
+        <div className="m-4 p-4 w-[219px] h-[340px] bg-red-200 hover:bg-red-300"></div>
+        <div className="m-4 p-4 w-[219px] h-[340px] bg-red-200 hover:bg-red-300"></div>
+        <div className="m-4 p-4 w-[219px] h-[340px] bg-red-200 hover:bg-red-300"></div>
+        <div className="m-4 p-4 w-[219px] h-[340px] bg-red-200 hover:bg-red-300"></div>
+        <div className="m-4 p-4 w-[219px] h-[340px] bg-red-200 hover:bg-red-300"></div>
+        <div className="m-4 p-4 w-[219px] h-[340px] bg-red-200 hover:bg-red-300"></div>
+        <div className="m-4 p-4 w-[219px] h-[340px] bg-red-200 hover:bg-red-300"></div>
+        <div className="m-4 p-4 w-[219px] h-[340px] bg-red-200 hover:bg-red-300"></div>
+        <div className="m-4 p-4 w-[219px] h-[340px] bg-red-200 hover:bg-red-300"></div>
+      </div>
     </div>
   );
 };
